366ba507689edb1ace763f7675f193a0002518b3
023PrashantSharma <44042702+023PrashantSharma@users.noreply.github.com>
2022-08-22 14:40:53 +0530

    update auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,6 @@ export type AmplifyDependentResourcesAttributes = {
         "testapp8bc8ea82": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
